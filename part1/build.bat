afa73e8475144bfb21c157c5c24602ea3976073f
doyle <doyle.github.2ifxi@simplelogin.com>
2023-03-07 01:12:17 +1100

    perfaware/part1: Handle positive value IMM->Reg

--- part1/build.bat
+++ part1/build.bat
@@ -15,7 +15,7 @@ copy /Y %script_dir%\listing_0040_challenge_movs %build_dir% 1>NUL
 REM Build
 REM ===========================================================================
 pushd %build_dir%
-cl %script_dir%\sim8086.c /W4 /WX /Z7 /nologo
+cl %script_dir%\sim8086.c /W4 /WX /Z7 /nologo || exit /b 1
 popd
 
 REM Tests
@@ -29,3 +29,8 @@ set listing_0038=%build_dir%\listing_0038_many_register_mov
 %build_dir%\sim8086.exe %listing_0038% > %listing_0038%_disassembled.asm
 nasm %listing_0038%_disassembled.asm
 fc %listing_0038% %listing_0038%_disassembled || exit /b 1
+
+set listing_0039=%build_dir%\listing_0039_more_movs
+%build_dir%\sim8086.exe %listing_0039% > %listing_0039%_disassembled.asm
+nasm %listing_0039%_disassembled.asm
+fc %listing_0039% %listing_0039%_disassembled || exit /b 1
